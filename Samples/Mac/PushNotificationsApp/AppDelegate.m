5110bc16bb56f04d045d63dea9065a4528041307
Max Konev <max@arello-mobile.com>
2016-04-12 19:06:48 -0400

    updated sample to use new SDK and guide

--- Samples/Mac/PushNotificationsApp/AppDelegate.m
+++ Samples/Mac/PushNotificationsApp/AppDelegate.m
@@ -4,7 +4,7 @@
 //
 
 #import "AppDelegate.h"
-#import <PushNotificationManager/PushNotificationManager.h>
+#import <Pushwoosh/PushNotificationManager.h>
 
 @implementation AppDelegate
 
@@ -12,26 +12,45 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
 	self.lblPushStatus.stringValue = @"Registering";
+	
+	[[PushNotificationManager pushManager] registerForPushNotifications];
+	[PushNotificationManager pushManager].delegate = self;
+	[[PushNotificationManager pushManager] handlePushReceived:[aNotification userInfo]];
+	[[PushNotificationManager pushManager] sendAppOpen];
+
 }
 
 #pragma mark -
-#pragma mark PushNotificationDelegate
 
-- (void)onPushAccepted:(PushNotificationManager *)pushManager withNotification:(NSDictionary *)pushNotification {
-	NSLog(@"Push accepted");
-
-	self.lblPushStatus.stringValue = @"Push received";
-	self.lblPushPayload.stringValue = pushNotification.description;
+// system push notification registration success callback, delegate to pushManager
+- (void)application:(NSApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+	[[PushNotificationManager pushManager] handlePushRegistration:deviceToken];
 }
 
-- (void)onDidRegisterForRemoteNotificationsWithDeviceToken:(NSString *)token {
+// system push notification registration error callback, delegate to pushManager
+- (void)application:(NSApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+	[[PushNotificationManager pushManager] handlePushRegistrationFailure:error];
+
+}
+
+// system push notifications callback, delegate to pushManager
+- (void)application:(NSApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+	[[PushNotificationManager pushManager] handlePushReceived:userInfo];
+}
+
+- (void) onDidRegisterForRemoteNotificationsWithDeviceToken:(NSString *)token {
 	self.lblPushStatus.stringValue = @"Success";
-	self.lblPushToken.stringValue = token;
+	self.lblPushToken.stringValue = [[PushNotificationManager pushManager] getPushToken];
 }
 
-- (void)onDidFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+- (void) onDidFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
 	self.lblPushStatus.stringValue = @"Error";
 	self.lblPushToken.stringValue = error.localizedDescription;
+}
+
+- (void) onPushAccepted:(PushNotificationManager *)pushManager withNotification:(NSDictionary *)pushNotification onStart:(BOOL)onStart {
+	self.lblPushStatus.stringValue = @"Push received";
+	self.lblPushPayload.stringValue = pushNotification.description;
 }
 
 #pragma mark -
